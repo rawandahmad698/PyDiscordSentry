64d444842bcf57f9d92d106ab1f3e7fc0f5e8fbd
Rawa <rawandahmad698@gmail.com>
2022-06-14 17:32:47 +0300

    Fix

--- Tests/raise_error.py
+++ Tests/raise_error.py
@@ -1,6 +1,6 @@
 import sentry_sdk
 
-sentry_sdk.init("https://Foo@pydiscordsentry.herokuapp.com/1", release="1.0.0", debug=True)
+sentry_sdk.init("https://Foo@awesomeapp.herokuapp.com/1", release="1.0.0", debug=True)
 
 
 # Raise an error
@@ -8,5 +8,5 @@ def raise_error():
     raise EOFError()
 
 
+sentry_sdk.capture_message("This is a test")
 raise Exception("This is a test")
-#sentry_sdk.capture_message("This is a test")
